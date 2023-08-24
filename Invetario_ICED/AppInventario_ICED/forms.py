bb94d28485651960bd0934a70df63fd510074acd
Dan2001afk <camolo.777@gmail.com>
2023-08-24 12:30:47 -0500

    funcionamiento de estilos al iniciar sesion

--- Invetario_ICED/AppInventario_ICED/forms.py
+++ Invetario_ICED/AppInventario_ICED/forms.py
@@ -1,10 +1,25 @@
 from django import forms
 from django.contrib.auth.forms import *
 from django.contrib.auth import get_user_model
+from typing import Any
+from .models import User
 
 User = get_user_model()
 
 class UserForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['Documento'].widget.attrs.update({
+            'required':'',
+             'name':'doc',
+             'id':'doc',
+             'type':'text',
+             'class':'form-input',
+             'placeholder':'Documento',
+             'maxlength':'20',
+             'minlength':'1'
+        })    
+
     rol=forms.CharField(max_length=100)
     imagen=forms.ImageField(required=False)
     Documento = forms.CharField(max_length=30)
--- Invetario_ICED/AppInventario_ICED/formsUsuario.py
+++ Invetario_ICED/AppInventario_ICED/formsUsuario.py
@@ -4,15 +4,7 @@ from .models import *
 
 
 class UsuariosForm(forms.ModelForm):
+    
     class Meta:
         model=Usuarios
         fields = ['Usu_Nombre', 'Usu_Apellido', 'Usu_tipo','Usu_Celular','Usu_Correo','Usu_Ficha']
-
-        widgets = {
-            'Usu_Nombre':forms.TextInput(attrs={'placeholder':'Usu_Nombre'}),
-            'Usu_Apellido':forms.TextInput(attrs={'placeholder':'Usu_Apellido'}),
-            'Usu_tipo':forms.TextInput(attrs={'placeholder':'Usu_tipo'}),
-            'Usu_Celular':forms.TextInput(attrs={'placeholder':'Usu_Celular'}),
-            'Usu_Correo':forms.TextInput(attrs={'placeholder':'Usu_Correo'}),
-            'Usu_Ficha':forms.TextInput(attrs={'placeholder':'Usu_Ficha'}),
-        }
