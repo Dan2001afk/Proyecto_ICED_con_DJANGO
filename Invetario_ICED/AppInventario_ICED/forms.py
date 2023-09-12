719c00552f209e12989c8124758a01f63e6a63cf
Dan2001afk <camolo.777@gmail.com>
2023-09-12 10:00:27 -0500

    Formulario para registrar formulario funcional con estilos y demas BY Daniel Gonzalez

--- Invetario_ICED/AppInventario_ICED/forms.py
+++ Invetario_ICED/AppInventario_ICED/forms.py
@@ -11,14 +11,79 @@ class UserForm(UserCreationForm):
         super().__init__(*args, **kwargs)
         self.fields['Documento'].widget.attrs.update({
             'required':'',
-             'name':'doc',
-             'id':'doc',
-             'type':'text',
-             'class':'form-input',
-             'placeholder':'Documento',
-             'maxlength':'20',
-             'minlength':'1'
+            'name':'doc',
+            'id':'doc',
+            'type':'text',
+            'class':'form-input',
+            'placeholder':'Documento',
+            'maxlength':'20',
+            'minlength':'1'
         })    
+        self.fields['username'].widget.attrs.update({
+            'required':'',
+            'name':'Nombre',
+            'id':'Nombre',
+            'type':'text',
+            'class':'form-input',
+            'placeholder':'nombre de usuario',
+            'maxlength':'20',
+            'minlength':'1'
+        })
+
+        self.fields['email'].widget.attrs.update({
+            'required':'',
+            'name':'email',
+            'id':'email',
+            'type':'text',
+            'class':'form-input',
+            'placeholder':'Correo',
+            'maxlength':'20',
+            'minlength':'1'
+        })
+
+        self.fields['password1'].widget.attrs.update({
+            'required':'',
+            'name':'password1',
+            'id':'password1',
+            'type':'text',
+            'class':'form-input',
+            'placeholder':'Contraseña',
+            'maxlength':'20',
+            'minlength':'1'
+        })
+
+        self.fields['password2'].widget.attrs.update({
+            'required':'',
+            'name':'password2',
+            'id':'password2',
+            'type':'text',
+            'class':'form-input',
+            'placeholder':'Confirmar Contraseña',
+            'maxlength':'20',
+            'minlength':'1'
+        })
+
+        self.fields['rol'].widget.attrs.update({
+            'required':'',
+            'name':'rol',
+            'id':'rol',
+            'type':'text',
+            'class':'form-input',
+            'placeholder':'Rol',
+            'maxlength':'20',
+            'minlength':'1'
+        })
+
+        self.fields['imagen'].widget.attrs.update({
+            'required':'',
+            'name':'imagen',
+            'id':'imagen',
+            'type':'text',
+            'class':'form-input',
+            'placeholder':'Imagen Perfil',
+            'maxlength':'20',
+            'minlength':'1'
+        })
 
     rol=forms.CharField(max_length=100)
     imagen=forms.ImageField(required=False)
